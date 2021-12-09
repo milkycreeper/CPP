b192cc3e1fce0c6832fe12537644f32fb4920e55
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2021-12-09 10:20:13 +0100

    Add files via upload

--- vjezba2.c
+++ vjezba2.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 void main(){
-    int strA=8;
-    int strB=5;
-    int strC=3;
-    printf("%d", strA*strB*strC);
+    int strA;
+    int strB;
+    int strC;
+    printf("Unes strance\n");
+    scanf("%d %d %d",&strA,&strB,&strC);
+    printf("Volumen je %d cm na trecu!",strA*strB*strC);
 }
