c0386420dceb2ccb705d4ef127911e0b24d98069
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2021-12-16 07:56:24 +0100

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y;
+    x=7;
+    y=9;
+
+    printf("%d + %d\n", x,y);
+    printf("%d - %d\n", x,y);
+    printf("%d * %d\n", x,y);
+    printf("%d / %d\n", x,y);
+    return 0;
+}
+
