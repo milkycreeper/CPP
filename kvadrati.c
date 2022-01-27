4aeb831530058299d3deda56415bdf09b4dc6027
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-01-27 08:28:40 +0100

    Add files via upload

---
+++ kvadrati.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a;
+    printf("Unesi broj a:\n");
+    scanf("%d",&a);
+
+    int b;
+    printf("Unesi broj b:\n");
+    scanf("%d",&b);
+
+    int c;
+    printf("Unesi broj c:\n");
+    scanf("%d",&c);
+
+    printf("Potencija broja a je: %d\n", a*a);
+    printf("Potencija broja b je: %d\n", b*b);
+    printf("Potencija broja c je: %d\n", c*c);
+
+    printf("Zbroj ovih brojeva je: %d\n", a*a+b*b+c*c);
+
+
+
+
+
+
+
+
+
+
+
+
+}
