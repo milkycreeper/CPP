44480297ccd264708d08a138e2840fb05011884f
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-02-03 10:33:02 +0100

    Add files via upload

---
+++ razlomak.c
@@ -0,0 +1,19 @@
+    #include<stdio.h>
+	#include<stdlib.h>
+	#include<math.h>
+
+	int main(){
+		int a;
+		int b;
+		int c;
+		int d;
+
+	printf("Unesi a,b,c i d\n");
+	scanf("%d %d %d %d", &a ,&b ,&c,&d);
+	int x=b==d?(a+c):c*b+a*d;
+	int y=b==d?d:d*b;
+	printf("Rezultat zbrajanja je: %d / %d\n",x,y);
+	printf("Rezultat zbrajanja je: %.2f\n",(float)x/y);
+
+
+	}
