145aa0498d48b034f975f63996d2e0a7454f9aa7
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-03-10 08:56:08 +0100

    Add files via upload

---
+++ parnepar1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int a;
+
+    printf ("Unesi broj\n");
+    scanf ("%d", &a);
+
+    if (a % 2 != 0);
+    {
+    printf("Broj je neparan");
+   {
+
+    if(a % 2 == 0)
+        printf("Broj je paran");
+   }
+    return 0;
+}
+}
