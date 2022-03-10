79cb62b32b12ca1754bb9d5042fb5bc6eab1f35d
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-03-10 09:09:04 +0100

    Update parnepar1.c

--- parnepar1.c
+++ parnepar1.c
@@ -8,14 +8,13 @@ int main(){
     printf ("Unesi broj\n");
     scanf ("%d", &a);
 
-    if (a % 2 != 0);
+    if (a % 2 == 1)
     {
     printf("Broj je neparan");
-   {
+    }
 
-    if(a % 2 == 0)
+    if(a % 2 == 0){
         printf("Broj je paran");
    }
     return 0;
-}
 }
