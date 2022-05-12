345d50a6dcc2c903cd96f4091b76c5da3fcd00f9
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-05-12 10:14:23 +0200

    Add files via upload

---
+++ zadatak za ispis u redke.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int i;
+    int x[6];
+
+    for(i=0;i<6;i++){      //upis podataka
+        printf("Unesi broj:");
+        scanf("%d", &x[i]);
+    }
+
+    printf("\n");
+
+    for(i=0;i<6;i++){     //ispis podataka
+        printf("%d\n", x[i]);
+    }
+return 0;
+
+
+
+
+}
