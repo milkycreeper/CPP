740c7d1a8b1cc4adda581263a66885e66ec3f5a7
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-05-26 10:15:56 +0200

    Add files via upload

--- meni polje.c
+++ meni polje.c
@@ -9,17 +9,23 @@ int main(){
     float sum=0;
     int i;
     int count=0;
+    int racunGotov=0;
 
     while(nestojeodabrano){
+            racunGotov=1;
     system("cls");
-    printf("Racun broj %d\t", ++count);
+    printf("Racun broj %d\t",++count );
     printf("Promet: %.2f\n", sum);
     printf("1. Kava: 120kn\n");
     printf("2. Sladoled: 1,5kn\n");
     printf("3. Govno: 25kn\n");
     printf("4. Krofna: 1500kn\n");
-    printf("Za prestanak unosa unesite bilo sto drugo!");
+    printf("Za prestanak unosa unesite bilo sto drugo!\n");
 
+
+    while(racunGotov){
+            printf("Za kraj racuna upisite gotovo\n");
+    printf("Odabir: ");
     scanf("%s", odabir);
 
     for(i=0;odabir[i]!='\0';i++)
@@ -28,8 +34,10 @@ int main(){
     if(!strcmp(odabir,"kava")|!strcmp(odabir,"1")) sum+=120;
     else if (!strcmp(odabir,"sladoled")|!strcmp(odabir,"2")) sum+=1.5;
     else if (!strcmp(odabir,"govno")|!strcmp(odabir,"3")) sum+=25;
-    else if (!strcmp(odabir,"kforna")|!strcmp(odabir,"4")) sum+=1500;
+    else if (!strcmp(odabir,"krofna")|!strcmp(odabir,"4")) sum+=1500;
+    else if (!strcmp(odabir,"gotovo"))racunGotov=0;
     else nestojeodabrano=0;
+    }
 }
 printf("Ukupan promet danas iznosi %.2f", sum);
 }
