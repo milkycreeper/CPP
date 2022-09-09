8c8017d6ced33b2fca8788a2094c076228347dbd
milkycreeper <95847363+milkycreeper@users.noreply.github.com>
2022-09-09 09:04:29 +0200

    Add files via upload

--- cetvrti4.c
+++ cetvrti4.c
@@ -1,3 +1,40 @@
 #include<stdio.h>
 
-int main()
+int main(){
+
+    int P1;
+    int P2;
+    int P3;
+    int P4;
+    int count=0;
+
+    printf("Unesi zadatke u  prvom tjednu: \n");
+    scanf("%d", &P1);
+    printf("Unesi zadatke u  drugom tjednu: \n");
+    scanf("%d", &P2);
+    printf("Unesi zadatke u  trecem tjednu: \n");
+    scanf("%d", &P3);
+    printf("Unesi zadatke u  cetvrtom tjednu: \n");
+    scanf("%d", &P4);
+
+    if(P1>=10){
+
+        count+=1;
+    }
+     if(P2>=10){
+
+        count+=1;
+    }
+      if(P3>=10){
+
+        count+=1;
+    }
+    if(P4>=10){
+
+        count+=1;
+    }
+
+    printf("Ivan je postigao svoj cilj u %d tjedna", count);
+
+
+}
